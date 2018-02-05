77be4dd04f879bb85129b17a913336e2c258f5d9
Zilgaro <verner_saaristo@msn.com>
2018-02-05 12:45:16 +0200

    Tehtävä 4.2

--- index.js
+++ index.js
@@ -4,45 +4,25 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-module.exports = Blog
-
-app.use(cors())
-app.use(bodyParser.json())
+const middleware = require('./utils/middleware')
+const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+app.use(cors())
+app.use(bodyParser.json())
+app.use(middleware.logger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.error)
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
 mongoose.Promise = global.Promise
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
